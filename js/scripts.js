67da6b3dd8dc6e3b8996e8272541e1f8aa0168ca
John Blalock <simpledimplejohn@gmail.com>
2021-04-30 16:05:53 -0700

    add color choice connected to images

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,27 @@ $(document).ready(function() {
     $("form#form").submit(function(event) {
         event.preventDefault();
         console.log("button clicked.") //debug
-        let language = $("#language").val();
-
+        let language = $("#color").val();
         console.log(language); //debug
-
-
+        
+        if (language == 'red') {
+            $(".c_sharp").show();
+        }
+        if (language == 'blue') {
+            $(".go").show();
+        }
+        if (language == 'brown') {
+            $(".javascript").show();
+        }
+        if (language == 'yellow') {
+            $(".ruby").show();
+        }
+        if (language == 'green') {
+            $(".rust").show();
+        }
+        if (language == 'orange') {
+            $(".swift").show();
+        }
 
 
     });
